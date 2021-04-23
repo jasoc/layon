9423eec816ea6642e19e88a7e1f69c76078741b7
3r1xon <erixonconsoli@hotmail.it>
2021-04-24 01:13:46 +0200

    Electron now works, if you want to build the app just type 'npm run electron-build' if you want to check all the commands you can see the package.json file

---
+++ layon/main.js
@@ -0,0 +1,50 @@
+const { app, BrowserWindow } = require('electron');
+const url = require("url");
+const path = require("path");
+
+let win;
+
+let mainWindow = () => {
+
+    win = new BrowserWindow({
+        width: 1920,
+        height: 1080,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+
+    win.loadURL(
+        url.format({
+            pathname: path.join(__dirname, `/dist/index.html`),
+            protocol: "file:",
+            slashes: true
+        })
+    );
+
+    // DevTools
+    // win.webContents.openDevTools()
+
+    win.on('closed', () => {
+        win = null;
+    });
+}
+
+// Create window on electron initialization
+app.on('ready', mainWindow)
+
+// Quit when all windows are closed
+app.on('window-all-closed', () => {
+
+    if(process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+
+app.on('activate', () => {
+
+    if(win === null) {
+        mainWindow();
+    }
+});
