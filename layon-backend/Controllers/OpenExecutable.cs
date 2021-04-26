c5b05607bb3ed3127d5c825b559beae0fd80d5db
3r1xon <erixonconsoli@hotmail.it>
2021-04-26 11:50:20 +0200

    Coding backend

--- layon-backend/Controllers/OpenExecutable.cs
+++ layon-backend/Controllers/OpenExecutable.cs
@@ -5,8 +5,11 @@ using System.Collections;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.Extensions.Configuration;
+using System.Linq;
 
 using System.Diagnostics;
+
+using Launcher.Models;
 
 
 
@@ -25,32 +28,53 @@ namespace aspnetcoreapp.Controllers
         }
 
         [HttpGet]
-        public string[] SearchForGames() {
+        public FileInfo[] SearchForGames() {
+            // Folders that should be found: Steam[DONE], Origin Games[TO BE DONE]
+            Launchers Launcher = new Launchers();
+
+
+            // Drives name
+            DriveInfo[] Drives = DriveInfo.GetDrives();
+            // All the Games directories each drive will have
             List<string> Games = new List<string>();
-            string[] Directories = Directory.GetDirectories(@"C:\", "p*", SearchOption.TopDirectoryOnly);
-            string PathToApps = "";
 
-            foreach(string dir in Directories) {
-                if(dir.Contains("x86")) {
-                    PathToApps = dir;
-                    break;
+            int index = 0;
+            foreach(DriveInfo i in Drives) {
+                // Check the first drive
+                string[] temp = Directory.GetDirectories(i.ToString());
+                string path = "";
+
+                // All the MAIN directories on each drive contained by temp
+                for(int j = 0; j < temp.Length; j++) {
+                    try {
+                        string[] _ = Directory.GetDirectories(temp[j]);
+                        for(int k = 0; k < _.Length; k++) {
+                            Console.WriteLine(_[k]);
+                            if(_[k].Contains("Steam")) {
+                                path = _[k].ToString() + @"\steamapps\common";
+                                Launcher.Steam = Directory.GetDirectories(path);
+                            }
+                        }
+                    }
+                    catch(Exception e) {
+                        Console.WriteLine($"In catch 54 {e}");
+                    }
                 }
+                index++;
             }
 
-            if(System.IO.Directory.Exists($@"{PathToApps}\Steam\steamapps\common"))
-            {
-                PathToApps = $@"{PathToApps}\Steam\steamapps\common";
-                Directories = Directory.GetDirectories(PathToApps);
-                
-                foreach(string dir in Directories) {
-                    Console.WriteLine(dir);
-                }
+            Console.WriteLine("----------------><-----------------");
 
-                return new string[] {"Directory found"};
-            }
-            else {
-                throw new Exception("Steam folder not found");
-            }
+            DirectoryInfo d = new DirectoryInfo(Launcher.Steam[0]);
+            FileInfo[] Files = d.GetFiles("*.exe");
+            Console.WriteLine(Files[0]);
+            return Files;
+
+            // Note per domani
+            // DEVO TORNARE UNA LISTA DI STRINGHE, OGNUNA CORRISPONDENTE AL GIOCO DA AVVIARE
+            // QUANDO DAL FRONTEND AVVIERO` L'APP L'INDEX DELL'ARRAY SARA` IL PATH DEL GIOCO
+            // DA MANDARE AL METODO PROCESS.START CHE AVVIERA` IL GIOCO.
+
         }
     }
 }
---
+++ layon-backend/Models/Launcher.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Collections;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+
+using System.Diagnostics;
+
+
+
+namespace Launcher.Models
+{
+    public class Launchers  {
+
+        public string[] Steam;
+        public string[] Origin;
+        public string[] EpicGames;
+        public string[] BattleState;
+        public string[] BattleNet;
+        public string[] RockstarGames;
+
+    }
+    
+}
